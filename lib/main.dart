95c77346b58f2aa287a857581228920b8fd7a0b9
alex-ivankova766 <ivankova.alexandra766@gmail.com>
2024-02-06 10:45:47 +0300

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app_with_clean_architecture_and_the_bloc_pattern/src/config/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Social media application',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
+      theme: CustomTheme().theme(),
       home: const Scaffold(body: Center(child: Text('Development has begun'))),
     );
   }
