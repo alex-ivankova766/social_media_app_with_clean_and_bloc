ec7962bb69429a263c8f6751ccf5b51643b20956
alex-ivankova766 <ivankova.alexandra766@gmail.com>
2024-02-20 20:26:04 +0500

    finish login screen and added sign up screen

--- lib/src/features/auth/presentation/views/login_screen.dart
+++ lib/src/features/auth/presentation/views/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../shared/presentation/widgets/widgets.dart';
 
@@ -9,22 +10,49 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: SafeArea(
+      body: const SafeArea(
           child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(20.0),
         child: Column(
           children: [
             Spacer(flex: 3),
             _Username(),
-            const SizedBox(
+            SizedBox(
               height: 10,
             ),
             _Password(),
-            Spacer(flex: 2)
+            SizedBox(
+              height: 10,
+            ),
+            _LoginButton(),
+            Spacer(flex: 2),
+            _SignUpRedirect()
           ],
         ),
       )),
     );
+  }
+}
+
+class _SignUpRedirect extends StatelessWidget {
+  const _SignUpRedirect();
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          context.goNamed('signup');
+        },
+        child: RichText(
+            text: TextSpan(children: [
+          const TextSpan(text: 'Don\'t you have an account? '),
+          TextSpan(
+              text: 'Signup',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(fontWeight: FontWeight.bold)),
+        ])));
   }
 }
 
@@ -49,5 +77,29 @@ class _Password extends StatelessWidget {
       labelText: 'Password',
       obscureText: true,
     );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            backgroundColor: Colors.white,
+            minimumSize: const Size(100, 50)),
+        child: Text(
+          'Login',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(color: Colors.black),
+        ));
   }
 }
