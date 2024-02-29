84c4c79a05fecee359f92451dc5789f35ff20766
alex-ivankova766 <ivankova.alexandra766@gmail.com>
2024-02-29 14:50:05 +0500

    post model added

---
+++ lib/src/shared/data/datasources/post_data.dart
@@ -0,0 +1,37 @@
+List<Map<String, dynamic>> posts = [
+  {
+    'id': 'post_1',
+    'userId': 'user_1',
+    'caption':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui dolor, pulvinar ut odio a, facilisis dignissim risus.',
+    'assetPath': 'assets/videos/video_1.mp4',
+  },
+  {
+    'id': 'post_2',
+    'userId': 'user_1',
+    'caption':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui dolor, pulvinar ut odio a, facilisis dignissim risus.',
+    'assetPath': 'assets/videos/video_2.mp4',
+  },
+  {
+    'id': 'post_3',
+    'userId': 'user_1',
+    'caption':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui dolor, pulvinar ut odio a, facilisis dignissim risus.',
+    'assetPath': 'assets/videos/video_3.mp4',
+  },
+  {
+    'id': 'post_4',
+    'userId': 'user_2',
+    'caption':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui dolor, pulvinar ut odio a, facilisis dignissim risus.',
+    'assetPath': 'assets/videos/video_4.mp4',
+  },
+  {
+    'id': 'post_5',
+    'userId': 'user_2',
+    'caption':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui dolor, pulvinar ut odio a, facilisis dignissim risus.',
+    'assetPath': 'assets/videos/video_5.mp4',
+  },
+];
