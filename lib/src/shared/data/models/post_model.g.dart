3c9b4cc5c57d6cc4bef0476d38fa19aebb3eb789
alex-ivankova766 <ivankova.alexandra766@gmail.com>
2024-03-17 09:45:36 +0500

    type adapters are added and g.dart files are generated

---
+++ lib/src/shared/data/models/post_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PostModelAdapter extends TypeAdapter<PostModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  PostModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PostModel(
+      id: fields[0] as String,
+      user: fields[1] as UserModel,
+      caption: fields[2] as String,
+      assetPath: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PostModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.user)
+      ..writeByte(2)
+      ..write(obj.caption)
+      ..writeByte(3)
+      ..write(obj.assetPath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PostModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
